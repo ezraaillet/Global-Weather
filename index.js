a88075e7f08c02b1238b84c30e73a89368ab1eac
ezraaillet <ezraaillet204@gmail.com>
2022-04-16 12:29:56 -0500

    fixed background image api

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let weather = {
         document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
         document.querySelector(".wind").innerText = `Wind Speed: ${speed}mph`;
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/random?" + name + "')"
     },
     search: function() {
         this.fetchWeather(document.querySelector(".search-bar").value);
